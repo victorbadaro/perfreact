47d0ba7c8c31b039969094979b3dbfb06d82de8c
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-12-03 16:16:27 -0300

    feat: add a virtualized list to render products

--- src/components/search-results.tsx
+++ src/components/search-results.tsx
@@ -1,3 +1,4 @@
+import { List, type ListRowRenderer } from 'react-virtualized';
 import { ProductItem } from './product-item';
 
 interface SearchResultsProps {
@@ -12,13 +13,23 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ totalPrice, results, onAddToWishList }: SearchResultsProps) {
+	const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+		return (
+			<div key={key} style={style}>
+				<ProductItem product={results[index]} onAddToWishList={onAddToWishList} />
+			</div>
+		);
+	};
+
 	return (
 		<div>
 			<h2>{totalPrice}</h2>
 
-			{results.map((product) => (
+			{/* {results.map((product) => (
 				<ProductItem key={product.id} product={product} onAddToWishList={onAddToWishList} />
-			))}
+			))} */}
+
+			<List width={900} height={300} rowHeight={30} overscanRowCount={5} rowCount={results.length} rowRenderer={rowRenderer} />
 		</div>
 	);
 }
